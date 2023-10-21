2e56ad3b26c186e5e65db0301bc162d963e96c56
keiwv <141773283+keiwv@users.noreply.github.com>
2023-10-21 01:46:50 -0700

    modified:   PVBI_Act9_1_932/Frijoles.h 	modified:   PVBI_Act9_1_932/PVBI_Act9_1_932.c 	deleted:    PVBI_Act9_1_932/data.h

--- PVBI_Act9_1_932/Frijoles.h
+++ PVBI_Act9_1_932/Frijoles.h
@@ -1,29 +1,29 @@
 //*******LIBRARIES*************
 #include <stdlib.h>
 #include <stdio.h>
-#include <wchar.h>
-
+#include <conio.h>
+#include <string.h>
 //*******PROTOTYPE FUNCTIONS *********
 int valid(char msge[], int ri, int rf);
 int existElem(int vector[], int longi, int num);
-int counter(wchar_t array[]);
+int counter(char array[]);
 void mayus(char array[]);
 void reverse(char array[]);
 void line(char array[]);
 void descending(char array[]);
 void reverseDescending(char array[]);
 void noVowels(char array[]);
-int vowels(wchar_t array[]);
+int vowels(char array[]);
 void minus(char array[]);
 void capital(char array[]);
 void noSpace(char array[]);
-int alfaSpace(wchar_t array[]);
+int alfaSpace(char array[]);
 void alfaSpaceValid(char array[]);
 int palindrome(char array[]);
 void printArr(char array[]);
 void fillVectorNoRepeat(int vect[], int n, int ri, int rf);
 void printArNum(char array[], int num);
-void ask(wchar_t array[], int size);
+void ask(char array[]);
 
 //******* FUNCTIONS DEVELOPMENT *******
 int valid(char msge[], int ri, int rf)
@@ -54,7 +54,7 @@ int existElem(int vector[], int longi, int num)
     return -1;
 }
 
-int counter(wchar_t array[])
+int counter(char array[])
 {
     int length;
     length = 0;
@@ -100,28 +100,32 @@ void noVowels(char array[])
     printf("\n");
 }
 
-int vowels(wchar_t array[])
+int vowels(char array[])
 {
     int i;
     for (i = 0; array[i] != '\0'; i++)
     {
-        if (array[i] == L'A')
+        if (array[i] == 'X')
         {
             return i;
         }
-        if (array[i] == L'E')
+        if (array[i] == 'A')
         {
             return i;
         }
-        if (array[i] == L'I')
+        if (array[i] == 'E')
         {
             return i;
         }
-        if (array[i] == L'O')
+        if (array[i] == 'I')
         {
             return i;
         }
-        if (array[i] == L'U')
+        if (array[i] == 'O')
+        {
+            return i;
+        }
+        if (array[i] == 'U')
         {
             return i;
         }
@@ -197,26 +201,65 @@ void noSpace(char array[])
     }
 }
 
-int alfaSpace(wchar_t cadena[])
+int alfaSpace(char cadena[])
 {
-    int i;
-    int exist = 0;
-    for (i = 0; cadena[i] != L'\0'; i++)
+    fflush(stdin);
+    gets(cadena);
+    int i = 0;
+    if (cadena[0] == ' ')
     {
-        if (i > 0)
+        return -1; // Space at the start has been found
+    }
+    if (cadena[0] == '\0')
+    {
+        return -1; // No characters has been found
+    }
+
+    for (i = 0; cadena[i] != '\0'; i++)
+    {
+        if (cadena[i] == -92 || cadena[i] == -91 || cadena[i] == '/' || cadena[i] == '-')
         {
-            exist = 1;
+            cadena[i] = 'X';
         }
     }
-    if (exist == 0)
+    mayus(cadena);
+    i = 0;
+    while (cadena[i] != '\0')
     {
-        return -1;
+        if ((cadena[i] >= 'A' && cadena[i] <= 'Z'))
+        {
+            // Mayus allowed
+        }
+        else
+        {
+            if ((cadena[i] >= 'a' && cadena[i] <= 'z'))
+            {
+                return -1; // Minus not allowed
+            }
+            else
+            {
+                if (cadena[i] == ' ')
+                {
+                    if (cadena[i + 1] == ' ')
+                    {
+                        return -1; // Double Spaces not allowed
+                    }
+                }
+                else
+                {
+                    cadena[i] = 'X';
+                }
+            }
+        }
+        i++;
     }
-    if (cadena[0] == L' ')
+
+    if (cadena[i - 1] == ' ')
     {
-        return -1;
+        return -1; // It ends with a spaces (not allowed)
     }
-    return 1; // La cadena está vacía o solo contiene espacios en blanco
+    printf("%s", cadena);
+    return 1; // Return 1 if it
 }
 
 void printArr(char array[])
@@ -319,8 +362,8 @@ void printArNum(char array[], int num)
     }
 }
 
-void ask(wchar_t array[], int size)
+void ask(char array[])
 {
     fflush(stdin);
-    fgetws(array, size, stdin);
+    gets(array);
 }
--- PVBI_Act9_1_932/PVBI_Act9_1_932.c
+++ PVBI_Act9_1_932/PVBI_Act9_1_932.c
@@ -4,26 +4,25 @@
 
 //**** LIBRARIES ****
 #include "Frijoles.h"
-#include <wchar.h>
-#include <string.h>
-#include <locale.h>
 //*******************
 
 //**** PROTOTYPE FUNCTIONS ***
 void menu();
 int msge_menu();
 
-void askNames(wchar_t name[]);
-void askLastName1(wchar_t LastName1[]);
-void askLastName2(wchar_t LastName2[]);
+void askNames(char name[]);
+void askLastName1(char LastName1[]);
+void askLastName2(char LastName2[]);
 int askSex();
-int askState();
-void printCURP(wchar_t name[], wchar_t LastName1[], wchar_t LastName2[], int day, int month, int year, int sex, int state);
-void printBirthday(int day, int month, int year); 
+void askState(char state[]);
+void printCURP(char name[], char LastName1[], char LastName2[], int day, int month, int year, int sex, char state[]);
+// AUXILIAR FUNCTIONS
+void displayStates(char state[]);
+char getConsonant(char array[]);
+char compound(char array[]);
 //**** MAIN FUNCTIONS ****
 int main()
 {
-    setlocale(LC_ALL, "");
     menu();
     return 0;
 }
@@ -32,10 +31,10 @@ int main()
 void menu()
 {
     int op;
-    wchar_t name[50], LastName1[30], LastName2[30];
+    char name[50], LastName1[30], LastName2[30];
     int day, month, year;
     int sex;
-    int state;
+    char state[4];
     do
     {
         op = msge_menu();
@@ -45,28 +44,28 @@ void menu()
         case 1:
             askNames(name);
             break;
-            case 2:
-                askLastName1(LastName1);
-                break;
-            case 3:
-                askLastName2(LastName2);
-                break;
-            case 4:
-                day = valid("Ingresa el dia de tu nacimiento: ", 1, 31);
-                month = valid("Ingresa el mes de tu nacimiento: ", 1, 12);
-                year = valid("Ingresa el anio de tu nacimiento: ", 1894, 2023);
-                year = year % 100;
-                printf("Se han guardado correctamente la fecha de nacimiento.\n");
-                break;
-            case 5:
-                sex = askSex();
-                break;
-            case 6:
-                state = askState();
-                break;
-            case 7:
-                printCURP(name, LastName1, LastName2, day, month, year, sex, state);
-                break;
+        case 2:
+            askLastName1(LastName1);
+            break;
+        case 3:
+            askLastName2(LastName2);
+            break;
+        case 4:
+            day = valid("Ingresa el dia de tu nacimiento: ", 1, 31);
+            month = valid("Ingresa el mes de tu nacimiento: ", 1, 12);
+            year = valid("Ingresa el anio de tu nacimiento: ", 1894, 2023);
+            year = year % 100;
+            printf("Se han guardado correctamente la fecha de nacimiento.\n");
+            break;
+        case 5:
+            sex = askSex();
+            break;
+        case 6:
+            askState(state);
+            break;
+        case 7:
+            printCURP(name, LastName1, LastName2, day, month, year, sex, state);
+            break;
         }
         system("PAUSE");
     } while (op != 0);
@@ -83,7 +82,7 @@ int msge_menu()
     printArr("3.- Ingresa Apellido Materno\n");
     printArr("4.- Ingresa el Dia de Nacimiento\n");
     printArr("5.- Seleccionar sexo\n");
-    printArr("6.- Selecciona Estado\n");
+    printArr("6.- Lugar de nacimiento\n");
     printArr("7.- Imprimir CURP\n");
     printArr("0.- Salir\n");
     op = valid("Selecciona una opcion: ", 0, 7);
@@ -91,32 +90,27 @@ int msge_menu()
 }
 
 //**********
-void askNames(wchar_t name[])
+void askNames(char name[])
 {
-    int size = 50;
     do
     {
-        printArr("Por favor, ingresa tu(s) nombre(s): ");
-        ask(name, size);
+        printf("Introduce el nombre: ");
     } while (alfaSpace(name) != 1);
 }
 
-void askLastName1(wchar_t LastName1[])
+void askLastName1(char LastName1[])
 {
-    int size = 50;
     do
     {
         printArr("Por favor, ingresa el apellido paterno: ");
-        ask(LastName1, size);
     } while (alfaSpace(LastName1) != 1);
 }
-void askLastName2(wchar_t LastName2[])
+
+void askLastName2(char LastName2[])
 {
-    int size = 50;
     do
     {
         printArr("Por favor, ingresa el apellido materno: ");
-        ask(LastName2, size);
     } while (alfaSpace(LastName2) != 1);
 }
 
@@ -127,53 +121,205 @@ int askSex()
     sex = valid("Introduce el sexo del estudiante: ", 0, 1);
     return sex;
 }
-int askState()
+
+void askState(char state[])
 {
-    return valid("Introduce el estado: ", 1, 32);
+    int isForeign;
+    system("CLS");
+    isForeign = valid("¿Eres extranjero? (1.- Si, 0.- No): ", 0, 1);
+
+    if (isForeign)
+    {
+        strcpy(state, "NE"); // Asigna "NE" para extranjeros
+    }
+    else
+    {
+        system("CLS");
+        displayStates(state);
+    }
 }
 
-void printCURP(wchar_t name[], wchar_t LastName1[], wchar_t LastName2[], int day, int month, int year, int sex, int state)
+void printCURP(char name[], char LastName1[], char LastName2[], int day, int month, int year, int sex, char state[])
 {
     int temp;
-    wchar_t tempArray[3];
+    char tempArray[4];
     temp = vowels(LastName1);
-    tempArray[0] = LastName1[0];
+
+    tempArray[0] = compound(LastName1);
     tempArray[1] = LastName1[temp];
-    tempArray[2] = LastName2[0];
-    tempArray[3] = name[0];
-    printf("%ls", tempArray);
-    printBirthday(day, month, year);
+    tempArray[2] = compound(LastName2);
+    tempArray[3] = compound(name);
+    tempArray[4] = '\0';
+    printf("%s", tempArray);
+    printf("%02d", year);
+    printf("%02d", month);
+    printf("%02d", day);
+    if (sex == 0)
+    {
+        printf("M");
+    }
+    else
+    {
+        printf("H");
+    }
+    printf("%s", state);
+    printf("%c", getConsonant(LastName1));
+    printf("%c", getConsonant(LastName2));
+    printf("%c", getConsonant(name));
     printf("\n");
-    system("PAUSE");
 }
 
 //***************************
-void printBirthday(int day, int month, int year)
+
+//**** AUXILIAR FUNCTIONS *****
+void displayStates(char state[])
 {
-    if (year <= 9)
+    int i;
+    int tempNum;
+    char states[32][50] =
+        {
+            "Aguascalientes",
+            "Baja California",
+            "Baja California Sur",
+            "Campeche",
+            "Chiapas",
+            "Chihuahua",
+            "Ciudad de Mexico",
+            "Coahuila",
+            "Colima",
+            "Durango",
+            "Estado de Mexico",
+            "Guanajuato",
+            "Guerrero",
+            "Hidalgo",
+            "Jalisco",
+            "Michoacan",
+            "Morelos",
+            "Nayarit",
+            "Nuevo Leon",
+            "Oaxaca",
+            "Puebla",
+            "Queretaro",
+            "Quintana Roo",
+            "San Luis Potosi",
+            "Sinaloa",
+            "Sonora",
+            "Tabasco",
+            "Tamaulipas",
+            "Tlaxcala",
+            "Veracruz",
+            "Yucatan",
+            "Zacatecas"};
+    char twoLetterState[32][3] =
+        {
+            "AG", // AGUASCALIENTES
+            "BC", // BAJA CALIFORNIA
+            "BS", // BAJA CALIFORNIA SUR
+            "CC", // CAMPECHE
+            "CL", // COAHUILA
+            "CM", // COLIMA
+            "CS", // CHIAPAS
+            "CH", // CHIHUAHUA
+            "DF", // DISTRITO FEDERAL
+            "DG", // DURANGO
+            "GT", // GUANAJUATO
+            "GR", // GUERRERO
+            "HG", // HIDALGO
+            "JC", // JALISCO
+            "MC", // MÉXICO
+            "MN", // MICHOACÝN
+            "MS", // MORELOS
+            "NT", // NAYARIT
+            "NL", // NUEVO LEÓN
+            "OC", // OAXACA
+            "PL", // PUEBLA
+            "QT", // QUERÉTARO
+            "QR", // QUINTANA ROO
+            "SP", // SAN LUIS POTOSÝ
+            "SL", // SINALOA
+            "SR", // SONORA
+            "TC", // TABASCO
+            "TS", // TAMAULIPAS
+            "TL", // TLAXCALA
+            "VZ", // VERACRUZ
+            "YN", // YUCATÝN
+            "ZS", // ZACATECAS
+        };
+
+    printf("LISTA DE ESTADOS DE LOS ESTADOS UNIDOS MEXICANOS\n");
+    for (i = 0; i < 32; i++)
     {
-        printf("0%d", year);
+        printf("%d.- %s\n", i + 1, states[i]);
     }
-    else
+    tempNum = valid("Introduce el estado en el que naciste: ", 1, 32);
+    strcpy(state, twoLetterState[tempNum - 1]);
+}
+
+char getConsonant(char array[])
+{
+    int i;
+    char tempword;
+    int flag = 0;
+    for (i = 0; array[i] != '\0'; i++)
     {
-        printf("%d", year);
+        tempword = array[i];
+
+        if ((tempword != 'A') && (tempword != 'E') && (tempword != 'I') && (tempword != 'O') && (tempword != 'U'))
+        {
+            if (flag == 1)
+            {
+                return tempword;
+            }
+            flag = 1;
+        }
     }
 
-    if (month <= 9)
-    {
-        printf("0%d", month);
-    }
-    else
-    {
-        printf("%d", month);
-    }
+    return -1;
+}
 
-    if (day <= 9)
+char compound(char array[])
+{
+    char contra[30][10] = {"MA", "MA.", "M.", "M", "JOSE", "J.", "J", "MARIA", "DA", "DAS", "DE", "DEL", "DER", "DI", "DIE", "DD", "EL", "LA", "LOS", "LAS", "LE", "LES", "MAC", "MC", "VAN", "VON", "Y"};
+    char temp[20];
+    int i = 0;
+    int k = 0;
+    int n = 0;
+    int flag = 0;
+    int l = 0;
+    while (array[i] != '\0' && (flag == 0))
     {
-        printf("0%d", day);
+        int j = 0;
+        while (array[k] != ' ' && array[k] != '\0')
+        {
+            temp[j] = array[k];
+            j++;
+            k++;
+        }
+        temp[j] = '\0';
+
+        while(flag != 1)
+        {
+            if (strcmp(temp, contra[l]) == 1)
+            {
+                flag = 1;
+            }
+            else
+            {
+                n += strlen(temp);
+            }
+            l++;
+        }
+        k++;
+        i++;
+    }
+    if (flag == 1)
+    {
+        printf("ASDASD");
+        return array[n];
     }
     else
     {
-        printf("%d", day);
+        printf("aaaa");
+        return array[0];
     }
 }
