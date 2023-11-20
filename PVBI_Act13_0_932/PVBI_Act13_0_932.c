0b7edf83c1c3ff403d52581fd1baff5801104a23
keiwv <141773283+keiwv@users.noreply.github.com>
2023-11-19 19:02:17 -0800

    deleted:    PVBI_Act13_0_932/Archivos de acceso aleatorio.c 	modified:   PVBI_Act13_0_932/PVBI_Act13_0_932.c 	new file:   PVBI_Act13_0_932/PVBI_RP13_PE.pdf 	new file:   PVBI_Act13_0_932/PVBI_RP13_PE_ANEXO.pdf 	modified:   PVBI_Act13_0_932/dataFunctions.h 	modified:   PVBI_Act13_0_932/output/PVBI_Act13_0_932.exe 	new file:   PVBI_Act13_0_932/output/datos.dll 	new file:   PVBI_Act13_0_932/output/datos.tmp 	new file:   PVBI_Act13_0_932/output/prueba_activos.txt 	new file:   PVBI_Act13_0_932/output/prueba_eliminados.txt

--- PVBI_Act13_0_932/PVBI_Act13_0_932.c
+++ PVBI_Act13_0_932/PVBI_Act13_0_932.c
@@ -1,5 +1,41 @@
 /*
+    Code Created: November 14th 2023 // Code Modified: November 19th 2023
 
+        Add: The program should be capable of adding 100 records to the records vector, generating the data 
+        automatically.
+
+        Edit Record: The program should search for a registration number in the vector using the most efficient
+         search method. Display the data as a record, inquire about which field to edit, update the data in the vector (only for active records).
+
+        Delete Record: The program should search for a registration number in the vector using the most efficient
+         search method. Use flags to choose the most appropriate method, print the record, and ask if the user wants to delete the record.
+
+        Search: The program should search for a registration number in the vector using the most efficient search
+         method. Use flags to choose the most appropriate method. Display the data as a record.
+
+        Sort: The program should sort the vector using the most efficient sorting method. Use flags to choose the
+         most appropriate method. Sorting will be based on the key field (registration number).
+
+        Print: The program should display all records in the vector and their current order (sorted or unsorted)
+         in a tabular form.
+
+        Generate Text File: The program should ask the user for the file name (without extension). It will generate
+         a text file with the provided name and a .txt extension. The data in the text file will be identical to the contents of the records vector (sorted or unsorted). 
+         The program can generate multiple files for output verification.
+
+        Show Text File: The program should ask the user for the file name (without extension). It will generate a file
+         with the provided name and a .txt extension, displaying the text file as it is.
+
+        Create Binary File: The program should create a binary file with the updated vector data, replace the base file,
+         create a backup of the previous file, and save it with the same name but with a .tmp extension. Display messages if the file cannot be created due to a lack of records in the vector.
+
+        Load Binary File: The program should load the vector with records from the binary file (the file can only be loaded
+         once and must be named data.dll; indicate if it does not exist).
+
+        Show Deleted: The program should display only the records marked as deleted (status 0) from the binary file, which were
+         previously marked as deleted.
+
+     PVBI_Act13_0_932
 */
 
 //***** LIBRARIES ***********
@@ -35,21 +71,31 @@ int deleteEmployee(TWrkr employee[], int position, int ordFlag);
 void searchEmployee(TWrkr employee[], int position, int ordFlag);
 int ordRegister(TWrkr employee[], int position, int ordFlag);
 void displayRegister(TWrkr employee[], int position);
+void generateTXT(TWrkr employee[], int position);
+void showTxTFile();
+void getBinaryFile(TWrkr employees[], int position);
+int readFileBinary(TWrkr employees[], int position);
+void showBinaryDlted();
+
 //****** USEFUL FUNCTIONS *********
 TWrkr getOneEmployee(TWrkr employee[], int position, int ordFlag);
 int existElem(int employeesEnrollment[], int longi, Tkey num);
 int binarySearch(int employeesEnrollment[], int left, int right, Tkey number);
 void displayRegEmp(TWrkr employee);
-void displayListEmp(TWrkr employee);
+void displayListEmp(TWrkr employee, int i);
 void getNumEnrollment(TWrkr employees[], int num1[], int position);
 void getNumCell(TWrkr employees[], int num1[], int position);
 int menuRegister(TWrkr employees[], int position, int index, int enrollments[], int ordFlag);
 int menuEditRegister();
+void getTXT(TWrkr employee[], int position, char fileName[], int flag);
+void addOneEmplyeeTxT(TWrkr employee, int position, FILE *fa);
+void loadFileEmployee(char fileName[]);
 
 void bubbleSort(TWrkr employees[], int n);
 void quicksort(TWrkr employees[], int low, int high);
 int partition(TWrkr employees[], int low, int high);
 void swap(TWrkr employees[], int i, int j);
+
 //****** MAIN FUNCTION ************
 int main()
 {
@@ -82,6 +128,8 @@ void menu()
     TWrkr employee[MAX_REGISTERS];
     int position = 0;
     int ordFlag = 0;
+    int tempPstion = 0;
+    int flagFile = 0;
     do
     {
         system("CLS");
@@ -108,6 +156,33 @@ void menu()
         case 6:
             displayRegister(employee, position);
             break;
+        case 7:
+            generateTXT(employee, position);
+            break;
+        case 8:
+            showTxTFile();
+            break;
+        case 9:
+            getBinaryFile(employee, position);
+            break;
+        case 10:
+            if (!flagFile)
+            {
+                tempPstion = position;
+                position = readFileBinary(employee, position);
+                if (tempPstion != position)
+                {
+                    flagFile = 1;
+                }
+            }
+            else
+            {
+                printf("El archivo ya ha sido cargado con anterioridad\n");
+            }
+            break;
+        case 11:
+            showBinaryDlted();
+            break;
         }
         system("PAUSE");
     } while (op != 0);
@@ -122,8 +197,8 @@ int addRegisters(TWrkr employee[], int position, int ordFlag)
         for (i = 0; i < GEN_REGISTERS; i++)
         {
             employee[position + i] = getOneEmployee(employee, position, ordFlag);
-            displayListEmp(employee[i]);
         }
+        printf("Se han aniadido %d registros\n", GEN_REGISTERS);
         position += GEN_REGISTERS;
     }
     else
@@ -247,7 +322,7 @@ int ordRegister(TWrkr employee[], int position, int ordFlag)
     {
         if (!(position == 0))
         {
-            if(position < 600)
+            if (position < 600)
             {
                 bubbleSort(employee, position);
             }
@@ -273,22 +348,143 @@ int ordRegister(TWrkr employee[], int position, int ordFlag)
 void displayRegister(TWrkr employee[], int position)
 {
     int i;
-    printf("%-10s %-12s %-15s %-20s %-15s %-10s %-30s %-20s %-5s %-10s\n",
-           "Status",
-           "Enrollment",
-           "Name",
-           "Last Name 1",
-           "Last Name 2",
-           "Sex",
-           "Job Position",
-           "State",
-           "Age",
-           "Cell Phone");
+    int j = 0;
+    printf("%-10s %-12s %-15s %-20s %-15s %-10s %-10s %-10s %-5s %-10s\n",
+           "No.",
+           "Matricula",
+           "Nombre",
+           "Ap. Paterno",
+           "Ao. Materno",
+           "Sexo",
+           "Posicion",
+           "Estado",
+           "Edad",
+           "Num. Cel");
     for (i = 0; i < position; i++)
     {
-        displayListEmp(employee[i]);
+        if (employee[i].status == 1)
+        {
+            displayListEmp(employee[i], j);
+            j++;
+        }
     }
 }
+
+void generateTXT(TWrkr employee[], int position)
+{
+    char fileName[30];
+    char temp[30];
+    if (position > 0)
+    {
+        do
+        {
+            ask("Ingresa el nombre con el que deseas guardarlo: ", fileName);
+        } while (alfaSpace(fileName) == -1 || strlen(fileName) > 30);
+        strcpy(temp, fileName);
+        strcat(fileName, "_activos.txt");
+        getTXT(employee, position, fileName, 1);
+        strcat(temp, "_eliminados.txt");
+        getTXT(employee, position, temp, 0);
+        printf("Archivos generados exitosamente!\n");
+    }
+    else
+    {
+        printf("El vector no tiene nada para guardar:)\n");
+    }
+}
+
+void showTxTFile()
+{
+    char fileName[30];
+    do
+    {
+        ask("Ingresa el nombre del archivo que deseas mostrar (txt): ", fileName);
+    } while (alfaSpace(fileName) == -1 || strlen(fileName) > 30);
+    strcat(fileName, ".txt");
+    loadFileEmployee(fileName);
+}
+
+void getBinaryFile(TWrkr employees[], int position)
+{
+    FILE *fa;
+
+    char nomArchivo[11] = "datos.tmp";
+
+    rename("datos.tmp", "datos.dll");
+
+    fa = fopen(nomArchivo, "wb");
+
+    if (fa)
+    {
+        fwrite(employees, sizeof(TWrkr), position, fa);
+        fclose(fa);
+        printf("Archivo creado exitosamente.\n");
+    }
+    else
+    {
+        printf("No se pudo crear el archivo\n");
+    }
+}
+
+int readFileBinary(TWrkr employees[], int position)
+{
+    FILE *fa;
+    TWrkr reg;
+
+    fa = fopen("datos.dll", "rb");
+
+    if (fa)
+    {
+        while (fread(&reg, sizeof(TWrkr), 1, fa))
+        {
+            employees[position] = reg;
+            position++;
+        }
+        printf("El archivo ha sido cargado correctamente\n");
+        fclose(fa);
+    }
+    else
+    {
+        printf("No se pudo abrir\n");
+    }
+    return position;
+}
+
+void showBinaryDlted()
+{
+    FILE *fa;
+    TWrkr reg;
+    int i = 0;
+    fa = fopen("datos.dll", "rb");
+    if (fa)
+    {
+        printf("%-10s %-12s %-15s %-20s %-15s %-10s %-10s %-10s %-5s %-10s\n",
+               "No.",
+               "Matricula",
+               "Nombre",
+               "Ap. Paterno",
+               "Ao. Materno",
+               "Sexo",
+               "Posicion",
+               "Estado",
+               "Edad",
+               "Num. Cel");
+        while (fread(&reg, sizeof(TWrkr), 1, fa))
+        {
+            if (reg.status == 0)
+            {
+                displayListEmp(reg, i);
+                i++;
+            }
+        }
+        fclose(fa);
+    }
+    else
+    {
+        printf("El archivo no pudo cargarse\n");
+    }
+}
+
 // ************* USEFUL FUNCTIONS DEVELOPEMENT ********
 TWrkr getOneEmployee(TWrkr employee[], int position, int ordFlag)
 {
@@ -395,10 +591,10 @@ void displayRegEmp(TWrkr employee)
     printf("Lug. Nacim:  %s\n", employee.state);
 }
 
-void displayListEmp(TWrkr employee)
+void displayListEmp(TWrkr employee, int i)
 {
-    printf("%-10d %-12d %-15s %-20s %-15s %-10s %-30s %-20s %-5d 646%-10d\n",
-           employee.status,
+    printf("%-10d %-12d %-15s %-20s %-15s %-10s %-10s %-10s %-5d 646%-10d\n",
+           i + 1,
            employee.enrollment,
            employee.name,
            employee.LastName1,
@@ -488,8 +684,107 @@ int menuRegister(TWrkr employees[], int position, int index, int enrollments[], 
     return ordFlag;
 }
 
-//******* ORDER FUNCTIONS ***********
+void getTXT(TWrkr employee[], int position, char fileName[], int flag)
+{
+    int i;
+    FILE *fa;
+    int j;
+    fa = fopen(fileName, "w");
 
+    if ((strcmp(fileName, "activos.txt") != 0) && (strcmp(fileName, "eliminados.txt") != 0))
+    {
+        fprintf(fa, "%-10s %-12s %-15s %-20s %-15s %-10s %-10s %-8s %-5s %-10s\n",
+                "No.",
+                "Matricula",
+                "Nombre",
+                "Ap. Paterno",
+                "Ao. Materno",
+                "Sexo",
+                "Posicion",
+                "Estado",
+                "Edad",
+                "Num. Cel");
+    }
+    j = 0;
+    for (i = 0; i < position; i++)
+    {
+        if (employee[i].status == flag)
+        {
+            addOneEmplyeeTxT(employee[i], j, fa);
+            j++;
+        }
+    }
+
+    fclose(fa);
+}
+
+void addOneEmplyeeTxT(TWrkr employee, int position, FILE *fa)
+{
+    fprintf(fa, "%-10d %-12d %-15s %-20s %-15s %-10s %-10s %-10s %-5d 646%-10d\n",
+            position + 1,
+            employee.enrollment,
+            employee.name,
+            employee.LastName1,
+            employee.LastName2,
+            employee.sex,
+            employee.JobPstion,
+            employee.state,
+            employee.age,
+            employee.cellPhone);
+}
+
+void loadFileEmployee(char fileName[])
+{
+    int i;
+    FILE *fa;
+    TWrkr reg;
+    int temp = 0;
+    int tempNo = 0;
+    char firstLine[200];
+    fa = fopen(fileName, "r");
+    i = 0;
+    if (fa)
+    {
+        fgets(firstLine, sizeof(firstLine), fa);
+        printf("%-10s %-12s %-15s %-20s %-15s %-10s %-10s %-10s %-5s %-10s\n",
+               "No.",
+               "Matricula",
+               "Nombre",
+               "Ap. Paterno",
+               "Ao. Materno",
+               "Sexo",
+               "Posicion",
+               "Estado",
+               "Edad",
+               "Num. Cel");
+        while (!feof(fa))
+        {
+            temp = fscanf(fa, "%d %d %s %s %s %s %s %s %d 646%d", &tempNo, 
+            &reg.enrollment, 
+            reg.name, 
+            reg.LastName1, 
+            reg.LastName2, 
+            reg.sex, 
+            reg.JobPstion, 
+            reg.state, 
+            &reg.age, 
+            &reg.cellPhone);
+
+            if (temp == 10)
+            {
+                displayListEmp(reg, i);
+                i++;
+            }
+        }
+    }
+    else
+    {
+        printf("El archivo no existe\n");
+    }
+    fclose(fa);
+}
+
+//******* ORDER FUNCTIONS ***********
 void swap(TWrkr employees[], int i, int j)
 {
     TWrkr temp = employees[i];
@@ -543,8 +838,6 @@ void bubbleSort(TWrkr employees[], int n)
         }
     }
 }
-
-
 
 /*
   /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\
